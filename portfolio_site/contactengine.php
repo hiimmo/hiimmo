1358bb172ee9543cd336c170e579cd44ced50831
hiimmo <olphert@hotmail.co.uk>
2015-06-01 14:06:20 +0100

    Initial commit
    Initial commit

---
+++ portfolio_site/contactengine.php
@@ -0,0 +1,39 @@
+<?php
+
+$EmailFrom = "olphert@hotmail.co.uk";
+$EmailTo = "olphert@hotmail.co.uk";
+$Subject = "Hiimmo Contact page";
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ portfolio_site/contactthanks.php
@@ -0,0 +1,123 @@
+<!-- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>A Nice & Simple Contact Form</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
+		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
+			
+			
+		<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="index.html">Back to Contact Form</a></p>
+	
+	</div>
+
+</body>
+
+</html> -->
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Hiimmo</title>
+	<meta name="description" content="Design Portfolio Website Created by Matthew Olphert">
+	<meta name="author" content="Matthew Olphert">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+<div class="band-header">
+	<div class="container">
+		<div class="sixteen columns">
+			<img src="./images/monogram.png" alt="logo">
+
+		</div>
+	</div><!-- container -->
+</div> <!--band end-->
+
+<div class="band-navigation">
+	<div class="container">
+		<div class="sixteen columns">
+			<ul>
+			    <li><a href="index.html">Hiimmo</a></li>
+			    <li><a href="about.html">About</a></li>
+			    <li><a href="portfolio.html">Portfolio</a></li>
+			    <li><a href="contact.html">Contact</a></li>
+			</ul>		
+		</div>
+	</div><!-- container -->
+</div> <!--band end-->
+
+<div class="band-content">
+	<div class="container">
+		<div class="sixteen columns portfolio">
+			<h4>Thanks for the message, I'll get back to you as soon as I can</h4><br />
+		
+		<p><a href="contact.html">Back to Contact Form</a></p>
+
+		</div>
+
+		<!-- <hr> -->
+	</div><!-- container -->
+</div> <!--band end-->
+
+<div class="band-footer">
+	<div class="container">
+		<div class="sixteen columns">
+			<h1></h1>
+			<p>Matthew Olphert <br> &copy; 2015</p>
+			<p></p>
+		</div>
+	</div><!-- container -->
+</div> <!--band end-->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
